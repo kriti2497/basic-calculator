32345de1e51fdbd463a11beb840b8d55cc3c4adc
kriti2497 <46481836+kriti2497@users.noreply.github.com>
2021-07-04 19:08:56 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'calculator';
+export class AppComponent implements OnInit {
+  // @ViewChild("input1", { static: true }) input1: ElementRef;
+  // @ViewChild("input2", { static: true }) input2: ElementRef;
+
+  inputVal1: number;
+  inputVal2: number;
+  result: number | boolean;
+
+  title = "calculator";
+  showDiv: boolean = false;
+  clicked: boolean = false;
+
+  ngOnInit() {
+    // console.log(this.inputVal1, this.inputVal2);
+  }
+
+  operation(op: string) {
+    this.result = 0;
+    this.clicked = true
+    if (this.inputVal1 && this.inputVal2) {
+      switch (op) {
+        case "add":
+          this.result = this.inputVal1 + this.inputVal2;
+          break;
+
+        case "sub":
+          this.result = this.inputVal1 - this.inputVal2;
+          break;
+
+        case "mul":
+          this.result = this.inputVal1 * this.inputVal2;
+          break;
+
+        case "div":
+          this.result = this.inputVal1 / this.inputVal2;
+          break;
+
+        case "mod":
+          this.result = this.inputVal1 % this.inputVal2;
+          break;
+
+        // case "and":
+        //   this.result = this.inputVal1 && this.inputVal2;
+        //   break;
+
+        // case "or":
+        //   this.result = this.inputVal1 || this.inputVal2;
+        //   break;
+      }
+      this.showDiv = true;
+    }
+  }
+
+  clearInputs() {
+    this.inputVal1 = null;
+    this.inputVal2 = null;
+    this.clicked = false;
+    this.showDiv = false;
+  }
 }
